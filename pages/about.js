3270ac409f819bf92746fc2cdf50016b3261ba12
abrambagus <you@example.com>
2022-01-05 16:08:10 +0700

    NextJS-Fundamental-05

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+export default function AboutPage() {
+  return (
+    <Layout title="about DJ events">
+      <h1>about</h1>
+      <p>This is app to find the latest DJ and other musical event</p>
+      <p>Version: 1.0.0</p>
+    </Layout>
+  );
+}
